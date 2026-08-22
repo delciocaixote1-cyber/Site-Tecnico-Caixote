7c015c8b8ec7305da2738ad6758d2055036d7e76
delciocaixote1-cyber <delciocaixote1@gmail.com>
2026-08-22 18:49:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,66 +1,43 @@
-const CACHE_NAME = "caixote-runner-v1";
+const CACHE_NAME = "caixote-runner-v2";
 
 const ARQUIVOS = [
-    "./",
-    "./jogo.html",
-    "./manifest.json"
+  "./",
+  "./jogo.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ARQUIVOS);
+    })
+  );
 
-    event.waitUntil(
-
-        caches.open(CACHE_NAME).then(function(cache) {
-
-            return cache.addAll(ARQUIVOS);
-
-        })
-
-    );
-
-    self.skipWaiting();
+  self.skipWaiting();
 });
-
 
 self.addEventListener("activate", function(event) {
-
-    event.waitUntil(
-
-        caches.keys().then(function(chaves) {
-
-            return Promise.all(
-
-                chaves.map(function(chave) {
-
-                    if (chave !== CACHE_NAME) {
-
-                        return caches.delete(chave);
-
-                    }
-
-                })
-
-            );
-
+  event.waitUntil(
+    caches.keys().then(function(chaves) {
+      return Promise.all(
+        chaves.map(function(chave) {
+          if (chave !== CACHE_NAME) {
+            return caches.delete(chave);
+          }
         })
+      );
+    })
+  );
 
-    );
-
-    self.clients.claim();
+  self.clients.claim();
 });
 
-
 self.addEventListener("fetch", function(event) {
-
-    event.respondWith(
-
-        caches.match(event.request).then(function(resposta) {
-
-            return resposta ||
-                   fetch(event.request);
-
-        })
-
-    );
-
+  event.respondWith(
+    caches.match(event.request).then(function(resposta) {
+      return resposta || fetch(event.request);
+    })
+  );
 });
